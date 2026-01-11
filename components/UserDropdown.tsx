863b8cb2f68348decfafcbc14958928034f24b52
Swaraj Ladke <swarajladke20@gmail.com>
2026-01-11 20:12:53 +0530

    Remove profile avatar image, show only user initial

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -8,7 +8,7 @@ import {
     DropdownMenuSeparator, 
     DropdownMenuTrigger, 
 } from "@/components/ui/dropdown-menu" 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar" 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar" 
 import {useRouter} from "next/navigation"; 
 import {Button} from "@/components/ui/button"; 
 import {LogOut} from "lucide-react"; 
@@ -27,12 +27,11 @@ const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: Stock
         <DropdownMenu> 
             <DropdownMenuTrigger asChild> 
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"> 
-                    <Avatar className="h-8 w-8"> 
-                        <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" /> 
-                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold"> 
-                            {user.name[0]} 
-                        </AvatarFallback> 
-                    </Avatar> 
+                    <Avatar className="h-8 w-8">
+                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                            {user.name[0]}
+                        </AvatarFallback>
+                    </Avatar>
                     <div className="hidden md:flex flex-col items-start"> 
                         <span className='text-base font-medium text-gray-400'> 
                             {user.name} 
@@ -43,12 +42,11 @@ const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: Stock
             <DropdownMenuContent className="text-gray-400"> 
                 <DropdownMenuLabel> 
                     <div className="flex relative items-center gap-3 py-2"> 
-                        <Avatar className="h-10 w-10"> 
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" /> 
-                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold"> 
-                                {user.name[0]} 
-                            </AvatarFallback> 
-                        </Avatar> 
+                        <Avatar className="h-10 w-10">
+                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                                {user.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
                         <div className="flex flex-col"> 
                             <span className='text-base font-medium text-gray-400'> 
                                 {user.name} 
